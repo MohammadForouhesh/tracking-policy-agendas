995a6ea30444bbdd4353e85338f1dafe631870cc
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-31 19:26:28 +0330

    Update xgb_clf.py

--- classifiers/xgb_clf.py
+++ classifiers/xgb_clf.py
@@ -5,14 +5,3 @@ from classifiers.meta_clf import MetaClf
 class XgbClf(MetaClf):
     def __init__(self, **kwargs):
         super().__init__(classifier_instance=XGBClassifier(n_estimators=300), **kwargs)
-
-    def inference(self, input_text: str):
-        vector = self.scaler.transform(self.emb.encode(input_text).reshape(1, -1))
-        return self.clf.predict(vector)[0]
-
-    def inference_proba(self, input_text: str):
-        vector = self.scaler.transform(self.emb.encode(input_text).reshape(1, -1))
-        return 1 if self.clf.predict_proba(vector)[0][1] >= 0.70 else 0
-
-    def predict_proba(self, array):
-        return list(map(int, self.clf.predict_proba(array)[:, 1] >= 0.70))
