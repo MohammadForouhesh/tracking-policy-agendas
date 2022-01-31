ff653b4628b7502758fd2a46f5f6e54417f0c3ad
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-31 19:02:26 +0330

    Update w2v_emb.py

--- word2vec/w2v_emb.py
+++ word2vec/w2v_emb.py
@@ -20,14 +20,17 @@ class W2VEmb:
         text_series = text_series.fillna('')
         self.tf_idf_transformation = self.tf_idf_transformer(text_series)
         self.wv2_corpus = W2VCorpus(text_series)
-        self.w2v_model = gensim.models.Word2Vec(sentences=self.wv2_corpus, min_count=1)
+        self.w2v_model = gensim.models.Word2Vec(sentences=self.wv2_corpus, min_count=3, vector_size=300)
 
     def __getitem__(self, text: str) -> np.ndarray:
-        return self.w2v_model.wv[text]
+        try:
+            return self.w2v_model.wv[text]
+        except:
+            return np.array([0 for _ in range(0, self.w2v_model.vector_size)])
 
     def tf_idf_transformer(self, text_series):
         tfidf = Pipeline([('count', CountVectorizer(encoding='utf-8', min_df=3,
-                                            max_features=100,
+                                            max_features=300,
                                             ngram_range=(1, 2))),
                           ('tfid', TfidfTransformer(sublinear_tf=True, norm='l2'))]).fit(text_series.ravel())
         return tfidf
