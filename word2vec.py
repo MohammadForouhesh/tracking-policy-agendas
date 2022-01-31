348301c69f82255118b806862d8af67e8e84ecbd
Mohammad Hossein Forouhesh Tehrani <Mohammad.Forouhesh@Outlook.com>
2022-01-31 11:05:02 +0330

    Update word2vec.py

---
+++ word2vec.py
@@ -0,0 +1,20 @@
+
+class Word2Vec():
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def most_similar_word(self, word):
+        pass
+
+    @staticmethod
+    def cosine_similarity(word1: str, word2: str) -> float:
+        pass
