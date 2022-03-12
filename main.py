5178be9291c26ed4c3cd4cefceb2253276b4bf65
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-12 18:37:55 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import pandas as pd
 from tqdm import tqdm
 from tracking_policy_agendas.classifiers.pa_clf import PAClf
 from tracking_policy_agendas.classifiers.xgb_clf import XgbClf
+from tracking_policy_agendas.classifiers.naive_bayes_clf import GNBClf
+from tracking_policy_agendas.classifiers.lasso_clf import LassoClf
 from tracking_policy_agendas.preprocess.preprocessing import remove_emoji, remove_redundant_characters
-from tracking_policy_agendas.word2vec.w2v_vis import reduce_dimensions, plot_with_matplotlib
 
 tqdm.pandas()
 
@@ -15,21 +16,22 @@ def inference_pipeline(model_path: str, input_text: str):
 
 def main(embedding_frame:pd.DataFrame, dataframe: pd.DataFrame, save_path: str):
     xgb = XgbClf(text_array=dataframe.prep_text, labels=dataframe.label, embedding_doc=embedding_frame.prep_text)
-    xgb.build()
-    # x_evals, y_evals, labels = reduce_dimensions(xgb.emb.w2v_model)
-    # plot_with_matplotlib(x_evals, y_evals, labels)
-    xgb.save_model(save_path)
-    xgb = PAClf(text_array=dataframe.prep_text, labels=dataframe.label, embedding_doc=embedding_frame.prep_text)
-    xgb.build()
-    x_evals, y_evals, labels = reduce_dimensions(xgb.emb.w2v_model)
-    plot_with_matplotlib(x_evals, y_evals, labels)
-    #xgb.save_model(save_path)
+    xgb.fit()
+    xgb.save_model('xgb_' + save_path)
+    pa = PAClf(text_array=dataframe.prep_text, labels=dataframe.label, embedding_doc=embedding_frame.prep_text)
+    pa.fit()
+    pa.save_model('pa_' + save_path)
+    lasso = LassoClf(text_array=dataframe.prep_text, labels=dataframe.label, embedding_doc=embedding_frame.prep_text)
+    lasso.fit()
+    lasso.save_model('lasso_' + save_path)
+    gnb = GNBClf(text_array=dataframe.prep_text, labels=dataframe.label, embedding_doc=embedding_frame.prep_text)
+    gnb.fit()
+    gnb.save_model('gnb_' + save_path)
 
 
 if __name__ == '__main__':
     df = pd.read_excel('vacine_sampling.xlsx')[['text', 'prep_text', 'label']]
-    emb_df = pd.read_excel('clf_status_id_dummy.xlsx')
-    print(emb_df.columns)
+    emb_df = pd.read_excel('clf_status_id_dummy.xlsx').sample(100000)
     emb_df = emb_df[emb_df.is__welfare == 1][['text', 'prep_text']]
     df['prep_text'] = df.prep_text.apply(lambda item: remove_redundant_characters(remove_emoji(item)))
     df = df.replace('', float('NaN')).dropna()
