feac9dd32a04a7ce0f927618fb228e25ca167dfc
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-12 18:55:21 +0330

    Update pa_clf.py

--- tracking_policy_agendas/classifiers/pa_clf.py
+++ tracking_policy_agendas/classifiers/pa_clf.py
@@ -1,7 +1,7 @@
 from sklearn.linear_model import PassiveAggressiveClassifier
-from .meta_clf import MetaClf
+from .meta_sklearn_clf import MetaSkLearnClf
 
 
-class PAClf(MetaClf):
+class PAClf(MetaSkLearnClf):
     def __init__(self, **kwargs):
         super().__init__(classifier_instance=PassiveAggressiveClassifier(max_iter=1000, loss='squared_hinge'), **kwargs)
