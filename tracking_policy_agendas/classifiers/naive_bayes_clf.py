45dc01951fd56563d2d24ee3c1059588695df5df
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-12 19:18:03 +0330

    Update naive_bayes_clf.py

--- tracking_policy_agendas/classifiers/naive_bayes_clf.py
+++ tracking_policy_agendas/classifiers/naive_bayes_clf.py
@@ -1,7 +1,7 @@
 from sklearn.naive_bayes import GaussianNB
-from .meta_clf import MetaClf
+from .meta_sklearn_clf import MetaSkLearnClf
 
 
-class GNBClf(MetaClf):
+class GNBClf(MetaSkLearnClf):
     def __init__(self, **kwargs):
         super().__init__(classifier_instance=GaussianNB(), **kwargs)
