039c28a2bb5b3708641394b8308f43c950bb4864
Mohammad Hossein Forouhesh Tehrani <17898264+MohammadForouhesh@users.noreply.github.com>
2022-03-13 19:25:20 +0330

    #2 lasso_clf

--- tracking_policy_agendas/classifiers/lasso_clf.py
+++ tracking_policy_agendas/classifiers/lasso_clf.py
@@ -1,3 +1,14 @@
+"""
+Lasso Classifier
+
+....................................................................................................
+MIT License
+Copyright (c) 2021-2023 AUT Iran, Mohammad H Forouhesh
+Copyright (c) 2021-2022 MetoData.ai, Mohammad H Forouhesh
+....................................................................................................
+This module encapsulate the Lasso classifier.
+"""
+
 from sklearn.linear_model import SGDClassifier
 from .meta_sklearn_clf import MetaSkLearnClf
 
